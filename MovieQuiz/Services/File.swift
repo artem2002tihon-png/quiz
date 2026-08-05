c85dd6522fa209ee912c944f93039339447f7b55
artemtihonov <artem2002tihon@gmail.com>
2026-08-05 23:06:04 +0300

    Исправлены все ошибки, налажено вз-ие между фабрикой и контроллером Реализовал паттерн делигат

---
+++ MovieQuiz/Services/File.swift
@@ -0,0 +1,11 @@
+weak var delegate: QuestionFactoryDelegate?
+
+func requestNextQuestion() {
+    guard let index = (0..<questions.count).randomElement() else {
+        delegate?.didReceiveNextQuestion(question: nil)
+        return
+    }
+
+    let question = questions[safe: index]
+    delegate?.didReceiveNextQuestion(question: question)
+}
