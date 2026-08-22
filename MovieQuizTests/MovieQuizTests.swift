7170099333627d0aa8167ba316a8f503504235ac
artemtihonov <artem2002tihon@gmail.com>
2026-08-22 13:18:31 +0300

    Наведен порядок в коде: убран лишний закомменченый код; QuestionFactoryDelegate вынес в отдельный extension; вынес настройки в отдельную функцию setup() и вызвана в viewDidLoad(); вынес константы в отдельный enum; убрал лишнюю скобку, из-за которой запрос некорректно строилс.

---
+++ MovieQuizTests/MovieQuizTests.swift
@@ -0,0 +1,18 @@
+//
+//  MovieQuizTests.swift
+//  MovieQuizTests
+//
+//  Created by Артём Тихонов on 21.08.2026.
+//
+
+import Testing
+
+struct MovieQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
