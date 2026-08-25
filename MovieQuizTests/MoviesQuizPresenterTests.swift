c34e26cfca37d96d323d8516265d864cbfc8e7bd
artemtihonov <artem2002tihon@gmail.com>
2026-08-26 00:51:40 +0300

    Проведён рефакторинг проекта. Составлены unit-тесты

---
+++ MovieQuizTests/MoviesQuizPresenterTests.swift
@@ -0,0 +1,24 @@
+import XCTest
+@testable import MovieQuiz
+
+final class MovieQuizViewControllerMock: MovieQuizViewControllerProtocol {
+    func show(quiz step: QuizStepViewModel) {}
+    func showLoadingIndicator() {}
+    func hideLoadingIndicator() {}
+    func enabledButtons(_ isEnabled: Bool) {}
+    func highlightImageBorder(isCorrectAnswer: Bool) {}
+}
+
+final class MovieQuizPresenterTests: XCTestCase {
+    func testPresenterConvertModel() throws {
+        let viewControllerMock = MovieQuizViewControllerMock()
+        let sut = MovieQuizPresenter(viewController: viewControllerMock)
+        let emptyData = Data()
+        let question = QuizQuestion(image: emptyData, text: "Question Text", correctAnswer: true)
+        let viewModel = sut.convert(model: question)
+        
+        XCTAssertEqual(viewModel.image, emptyData)
+        XCTAssertEqual(viewModel.question, "Question Text")
+        XCTAssertEqual(viewModel.questionNumber, "1/10")
+    }
+}
